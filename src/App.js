989ddd0e0dc1ac1abfbaf4d83220c7d843879dcb
Sergio <sergioval@gmail.com>
2022-05-08 23:42:32 -0500

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import sergio from '../../mi-blog/src/assets/sergio.jpg'
 
-const Header = (props) => {
+const SecretComponent = () => {
   return (
-  <h1>{props.name}'s Blog</h1>
+  <h1>
+    Secret information for authorized users only
+  </h1>
   )
 };
 
-const Blog = (props) => {
+const RegularComponent = () => {
   return (
-    <section>
-      <p>Hola, esta es mi {props.numeral} React App original</p>
-      <img  height={340} src={sergio} alt={sergio} />
-      <ul style={{ textAlign: 'left' }}>
-        {props.skills.map((skill) => 
-          (<li key={skill.id}>{skill.name}</li>))}; 
-      </ul>
-    </section>
+    <h1>
+      Everyone can see this Component
+    </h1>
   )
 };
 
-const Footer = (props) => {
+const App = (authorized) => {
   return (
-    <h3>Keep an eye {props.year}</h3>
-  )
-};
-
-const skills = [
-  "React",
-  "Redux",
-  "Node.js",
-  "MongoDB",
-  "MySQL",
-  "HTML5",
-  "CSS3",
-  "JavaScript",
-  "jQuery",
-  "Bootstrap",
-  "Tailwind CSS",
-];
-
-
-const skillsObject = skills.map((skill, i) => ({ id: i, name: skill }));
-console.log(skillsObject);
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header name='Sergio' />
-        <Blog numeral='segunda' skills={skillsObject} />
-        <Footer year={new Date().getFullYear()} />
-      </div>
-    );
-  }
+    <React.Fragment>
+      { authorized 
+        ? <SecretComponent /> 
+        : <RegularComponent />
+      }
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const AppTwo = () => {
-  return (
-    <p>
-      Hola, esta es mi React App original
-    </p>
-  )
-};
 
 ReactDOM.render(
-  <div>
-    <App />,
-    <AppTwo />,
-  </div>,
+    <App authorized={true} />,
   document.getElementById('root')
 );
